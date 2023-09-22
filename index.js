aeaacad2da4ac25d5bdb5804dfe71889446ec741
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-09-22 12:37:37 +0300

    Displayed all articles

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
-    layoutsDir: __dirname + '/views/layouts/',
+    layoutsDir: __dirname + '/views/layouts/'
 }))
 
 //setup static public directory
 app.use(express.static('public'))
 
+//app.use(express.json())
+//app.use(express.urlencoded({extended:true}))
+
 const mysql = require('mysql')
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -33,6 +37,17 @@ con.connect(err => {
     console.log("Connected to joga_mysql database!");
 });
 
-app.listen(3006, () => {
+//show all articles - index page
+app.get('/', (req, res) => {
+    let query = `SELECT * FROM article`;
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        res.render('index', {
+            articles: result
+        })
+    })
+});
+
+app.listen(3000, () => {
     console.log('web server is started')
 })
