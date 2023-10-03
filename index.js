719361bc255b1ee47ddd47c6bb57d2acdd62678e
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-10-03 21:30:15 +0300

    Created author controller and route, refactored application code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const path = require('path')
+const path = require('path');
 //add template engine
 const hbs = require('express-handlebars');
 //set up template engine directory and files extensions
@@ -11,42 +11,20 @@ app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
     layoutsDir: __dirname + '/views/layouts/'
-}))
+}));
 
 //setup static public directory
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-//app.use(express.json())
-//app.use(express.urlencoded({extended:true}))
-
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended:true}))
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 
 const articleRoutes = require('./routes/article');
-const con = require("./utils/db");
-app.use('/', articleRoutes)
-app.use('/article', articleRoutes)
-
-app.get('/author/:author_id', (req, res) => {
-    let get_author = `SELECT name FROM author WHERE id = ${req.params.author_id}`
-    let query = `SELECT article.id as 'id', article.name as 'name', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published' FROM article INNER JOIN author ON article.author_id = author.id WHERE author_id = ${req.params.author_id};`
-    let author = ''
-    con.query(get_author, (err, result) => {
-        if (err) throw err
-        if (result.length > 0) {
-            author = result[0].name
-        }
-    })
-    con.query(query, (err, result) => {
-        if (err) throw err
-        res.render('author', {
-            articles: result,
-            author: author
-        })
-    })
-})
-
+const authorRoutes = require('./routes/author');
+app.use('/', articleRoutes);
+app.use('/article', articleRoutes);
+app.use('/author', authorRoutes);
 
 app.listen(3000, () => {
     console.log('web server is started')
-})
+});
