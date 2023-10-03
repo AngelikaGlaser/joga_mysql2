7fe2e79008910f17ebd73dce60fad57185fb77b4
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-10-03 21:26:33 +0300

    Created article controller and route, refactored application code.

--- index.js
+++ index.js
@@ -19,51 +19,13 @@ app.use(express.static('public'))
 //app.use(express.json())
 //app.use(express.urlencoded({extended:true}))
 
-const mysql = require('mysql')
-
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 
-//create db connection
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "qwerty",
-    database: "joga_mysql"
-});
-
-con.connect(err => {
-    if (err) throw err;
-    console.log("Connected to joga_mysql database!");
-    let sqlQuery = 'SELECT name,author_id FROM article'
-    con.query(sqlQuery, (err, result) => {
-        if (err) throw err
-        console.log(result)
-    })
-});
-
-//show all articles - index page
-app.get('/', (req, res) => {
-    let query = "SELECT * FROM article";
-    con.query(query, (err, result) => {
-        if (err) throw err;
-        res.render('index', {
-            articles: result
-        })
-    })
-});
-
-//show article by this slug
-app.get('/article/:slug', (req, res) => {
-    let query = `SELECT article.id as 'id', article.name as 'name', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published', author.name as 'author', author.id as 'author_id' FROM article INNER JOIN author ON article.author_id = author.id WHERE slug = '${req.params.slug}'`;
-    con.query(query, (err, result) => {
-        if (err) throw err;
-        console.log(result)
-        res.render('article', {
-            article: result
-        })
-    })
-});
+const articleRoutes = require('./routes/article');
+const con = require("./utils/db");
+app.use('/', articleRoutes)
+app.use('/article', articleRoutes)
 
 app.get('/author/:author_id', (req, res) => {
     let get_author = `SELECT name FROM author WHERE id = ${req.params.author_id}`
