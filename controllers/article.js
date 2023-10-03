e585de6860e306c083f26095c306968f3ac0edde
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-10-03 23:42:15 +0300

    Create new article using the article model. - MVC

--- controllers/article.js
+++ controllers/article.js
@@ -14,7 +14,7 @@ const getAllArticles = (req, res) => {
             })
         }
     })
-}
+};
 
 // show article by a slug
 const getArticleBySlug = (req, res) => {
@@ -25,13 +25,47 @@ const getArticleBySlug = (req, res) => {
             })
         } else {
             console.log(data)
-            res.render('article', {article: data})
+            res.render('article',
+                {article: data})
+        }
+    })
+};
+
+//create new article
+const createNewArticle = (req, res) => {
+    //new article from POST data (example from form)
+    console.log('new article')
+
+    const newArticle = new Article ({
+        name: req.body.name,
+        slug: req.body.slug,
+        image: req.body.image,
+        body: req.body.body,
+        published: new Date().toISOString().slice(0, 19).replace('T', ' '),
+        author_id: req.body.author_id
+        })
+
+
+    Article.createNew(newArticle, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message : err.message || "Error occurred, while sending article data"
+            })
+        } else {
+            console.log(data)
+            res.redirect('/')
         }
     })
 }
+//display article form
+const showNewArticleForm = (req, res) => {
+    res.render('create')
+};
 
 // export ctrl functions
 module.exports = {
     getAllArticles,
-    getArticleBySlug
+    getArticleBySlug,
+    createNewArticle,
+    showNewArticleForm
 }
--- routes/article.js
+++ routes/article.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 // define article ctrl
-const articleCtrl = require('../controllers/article')
+const articleCtrl = require('../controllers/article');
 
 // use ctrl functions acc to route
-router.get('/', articleCtrl.getAllArticles)
-router.get('/article/:slug', articleCtrl.getArticleBySlug)
+router.get('/', articleCtrl.getAllArticles);
+router.get('/article/:slug', articleCtrl.getArticleBySlug);
+router.post('/article/create', articleCtrl.showNewArticleForm);
+router.post('/create', articleCtrl.createNewArticle);
 
 // export router for use in def application file
-module.exports = router
+module.exports = router;
