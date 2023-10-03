a81fcd57960fb7e1da0ea6be33099fc7c8b1ce07
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-10-03 22:30:46 +0300

    Displayed an article by slug using the article model. - MVC

--- controllers/article.js
+++ controllers/article.js
@@ -1,4 +1,5 @@
 const Article = require("../models/article.models")
+
 // show all articles (index)
 const getAllArticles = (req, res) => {
     Article.getAll((err, data) => {
@@ -17,12 +18,15 @@ const getAllArticles = (req, res) => {
 
 // show article by a slug
 const getArticleBySlug = (req, res) => {
-    let query = `SELECT article.id as 'id', article.name as 'name', article.slug as 'slug', article.image as 'image', article.body as 'body', article.published as 'published', author.name as 'author', author.id as 'author_id' FROM article INNER JOIN author ON article.author_id = author.id WHERE slug = '${req.params.slug}'`
-    con.query(query, (err, result) => {
-        if (err) throw err
-        res.render('article', {
-            article: result
-        })
+    Article.getBySlug(req.params.slug, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                message : err.message || "Error occurred retrieving article data"
+            })
+        } else {
+            console.log(data)
+            res.render('article', {article: data})
+        }
     })
 }
 
